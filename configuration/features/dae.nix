bdaaddfaecb5076d9919558190d4f93c6fcaa866
ChaosAttractor <lostattractor@gmail.com>
2024-06-01 14:46:01 +0800

    dae: do not hijack dns

--- configuration/features/dae.nix
+++ configuration/features/dae.nix
@@ -17,33 +17,21 @@
 
       check_interval: 360s # or 180?
 
-      # domain:   进行 SNI 嗅探, 会验证 SNI 是否可信, 通过发起一次DNS请求以判断SNI域名的解析结果是否和连接目标一致, 如果一致, 则覆盖连接目标为SNI域名
-      #           这要求 DAE 监听所有客户端的 DNS 请求并进行缓存, 并要求客户端每次发起连接时都重新进行 DNS 查询, 以保证连接目标和 DAE 的缓存一致
+      # domain:   进行 SNI 嗅探, 会验证 SNI 是否可信, 通过要求客户端每次进行连接时都产生 DNS 查询, 将 SNI/目标IP 与 DNS/查询结果 进行匹配
+      #           如果一致, 则覆盖连接目标为 SNI 目标, 这要求 DAE 监听所有客户端的 DNS 请求并进行缓存, 以保证客户端每次发起连接时都重新进行 DNS 查询
       #           DAE 会篡改 TTL 为 0, 因此只要 DNS 查询链路尊重上游 TTL, 即便下游有缓存也不会导致问题, 而缓存工作由 DAE 完成
       #           但显然这样的行为会使得 TTL 缓存完全失效, 增加网络负担和延迟
-      # domain+:  忽略 SNI 验证, 这使得 DNS 查询可以完全不经由 DAE 处理, 不过这可能导致提供了错误的SNI的情况下无法工作, 如APNS
-      # domain++: 忽略 SNI 验证的同时, 并以嗅探到的域名重新进行路由, 带来更好的准确度, 同时也意味着整个过程中，如果域名分流正确工作，可以抵御 DNS 污染
+      #           但这不会改变路由
+      # domain+:  忽略 SNI 验证, 这使得 DNS 查询可以完全不经由 DAE 处理, 不过这可能导致提供了错误的SNI的情况下无法工作, 如 Tor
+      # domain++: 忽略 SNI 验证的同时, 以嗅探到的域名重新进行路由, 带来更好的准确度, 同时也意味着整个过程中，如果域名分流正确工作，可以抵御 DNS 污染
       #           但也带来了更大的性能开销
-      # dial_mode: domain++  # SNI错误?
+      dial_mode: domain++  # SNI错误?
     }
 
     subscription {
       nexitally: '${config.sops.placeholder."dae/subscription/nexitally"}'
       imm: '${config.sops.placeholder."dae/subscription/imm"}'
       cnix: '${config.sops.placeholder."dae/subscription/cnix"}'
-    }
-
-    # See https://github.com/daeuniverse/dae/blob/main/docs/en/configuration/dns.md for full examples.
-    dns {
-      upstream {
-        googledns: 'tcp+udp://dns.google.com:53'
-      }
-      routing {
-        request {
-          qname(geosite:cn) -> asis
-          fallback: googledns
-        }
-      }
     }
 
     group {
@@ -81,14 +69,13 @@
 
     # See https://github.com/daeuniverse/dae/blob/main/docs/en/configuration/routing.md for full examples.
     routing {
-      ### Hijack only DNS queries from dnsmasq
-      # The rules here ensure that DNS traffic can be routed correctly outside of DNS hijacking
-      dport(53) && pname(dnsmasq) && !dip(geoip:private) && !dip(geoip:cn) -> proxy
-      dport(53) && pname(dnsmasq) -> direct
+      ## DO NOT hijack DNS (for ip/domain++ mode)
+      # Client -> DNSMASQ -> MOSDNS -(DOH)-> Upstream
       dport(53) -> must_rules
-      # TODO: Should be equivalent but doesn't work now
-      # https://github.com/daeuniverse/dae/issues/474
-      # dport(53) && !pname(systemd-resolved) && !pname(dnsmasq) -> must_rules
+      ## Or hijack and sniff verify (for domain mode, not working for router itself)
+      ## Client -(sniff verify)-> DNSMASQ -> MOSDNS -(no verify) -> Upstream
+      ## Client -(no verify)-> Upstream
+      # dport(53) && !dip(geoip:private) -> must_rules
 
       ## Bypass Private IPs
       dip(geoip:private) -> direct
