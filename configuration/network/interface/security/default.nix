48c168d56b103adeee4682fc954252b2fed376fb
ChaosAttractor <lostattractor@gmail.com>
2024-06-17 05:16:02 +0800

    nftables: rfc3489 full-cone nat

--- configuration/network/interface/security/default.nix
+++ configuration/network/interface/security/default.nix
@@ -7,7 +7,6 @@
         Address = [ "10.10.0.1/16" "fd23:3333:3333:10::1/64" ];
         IPv6SendRA = true;
         IPv6AcceptRA = false;  # 接受来自下游的 RA 是不必要的
-        IPMasquerade = "ipv4";
       };
       ipv6SendRAConfig = { Managed = true; OtherInformation = true; };
     };
