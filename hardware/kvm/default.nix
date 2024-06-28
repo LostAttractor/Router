46a7d58ec7dea9a09ae8e9aa911acb932126d22d
ChaosAttractor <lostattractor@gmail.com>
2024-06-29 05:39:54 +0800

    hardware/kvm:  do not set a login password

--- hardware/kvm/default.nix
+++ hardware/kvm/default.nix
@@ -17,4 +17,13 @@
 
   # Enable ZRAM
   zramSwap.enable = true;
+
+  # Allow the user to login as root without password.
+  users.users.root.initialHashedPassword = lib.mkOverride 150 "";
+
+  # Some more help text.
+  services.getty.helpLine = ''
+
+    Log in as "root" with an empty password.
+  '';
 }
