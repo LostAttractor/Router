d1db69716d205a5ea5838e5876b087ed0b351b66
ChaosAttractor <lostattractor@gmail.com>
2024-07-26 11:01:08 +0800

    using homelab flake

--- hardware/lxc/default.nix
+++
@@ -1,9 +0,0 @@
-{ modulesPath, ... }:
-{
-  imports = [ (modulesPath + "/virtualisation/lxc-container.nix") ];
-
-  # Supress systemd units that don't work because of LXC
-  systemd.suppressedSystemUnits = [ "sys-kernel-debug.mount" ];
-
-  networking.useHostResolvConf = false;
-}
