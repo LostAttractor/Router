1f89e35be3f3d381a27fa8ab79a2b2f2a647f8ae
ChaosAttractor <lostattractor@gmail.com>
2024-09-24 05:43:46 +0800

    tailscale: refactor & use nix to declarative

--- configuration/features/network/tailscale.nix
+++ configuration/features/network/tailscale.nix
@@ -1,9 +1,34 @@
-{ network, ... }:
+{ network, config, pkgs, ... }:
 {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
+    authKeyFile = config.sops.secrets.tailscale.path;
+    extraUpFlags = [
+      "--ssh"
+      "--advertise-exit-node"
+      "--accept-routes"
+      "--advertise-routes=10.0.0.0/16,10.1.0.0/16,10.10.0.0/16,10.100.0.0/16,10.255.0.0/16,fd7a:115c:a1e0:b1a:0:1:a00:0/104,fd23:3333:3333::/48"
+    ];
   };
+
+  # https://nixos.wiki/wiki/Networkd-dispatcher
+  services.networkd-dispatcher = {
+    enable = true;
+    rules = {
+      "50-tailscale" = {
+        onState = [ "routable" ];
+        # https://www.kernel.org/doc/html/latest/networking/segmentation-offloads.html
+        # https://tailscale.com/kb/1320/performance-best-practices#ethtool-configuration
+        script = ''
+          #!${pkgs.runtimeShell}
+          ${pkgs.ethtool}/bin/ethtool -K $IFACE rx-udp-gro-forwarding on rx-gro-list off
+        '';
+      };
+    };
+  };
+
+  sops.secrets.tailscale = {};
 
   network.interface.private.tailscale = network.interface.tailscale;
 }
