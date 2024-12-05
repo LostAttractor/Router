1a34f879b75ee96f926195eb97b7a770ae5a2cd3
ChaosAttractor <lostattractor@gmail.com>
2024-12-05 17:06:29 +0800

    flake: standalone mosdns from eh5's flake

---
+++ packages/mosdns/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
+buildGoModule rec {
+  pname = "mosdns";
+  version = "5.3.3";
+
+  src = fetchFromGitHub {
+    owner = "IrineSistiana";
+    repo = "mosdns";
+    rev = "v${version}";
+    sha256 = "sha256-nSqSfbpi91W28DaLjCsWlPiLe1gLVHeZnstktc/CLag=";
+  };
+
+  vendorHash = "sha256-RpvWkIDhHSNbdkpBCcXYbbvbmGiG15qyB5aEJRmg9s4=";
+  doCheck = false;
+
+  buildPhase = ''
+    buildFlagsArray=(-v -p $NIX_BUILD_CORES -ldflags="-s -w")
+    runHook preBuild
+    go build "''${buildFlagsArray[@]}" -o mosdns ./
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    install -Dm755 mosdns -t $out/bin
+  '';
+
+  meta = with lib; {
+    description = "A DNS proxy server";
+    homepage = "https://github.com/IrineSistiana/mosdns";
+    license = licenses.gpl3;
+  };
+}
